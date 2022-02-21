204a977b752791ea90d0adb7b677cc50e3d42933
Comp0707 <Comp0707@tehnikum.ket.local>
2022-02-21 10:57:17 +0300

    Добавьте файлы проекта.

---
+++ Перемешивание карт/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Перемешивание_карт
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Перемешивание карт");
+            Console.ReadKey();
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+
+            // Display each card that we've generated and placed in startingDeck in the console
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
+            Console.ReadKey();
+        }
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+    }
+}
